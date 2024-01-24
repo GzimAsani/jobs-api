491d71f5d7f05ac2b0e444a6db3f7bcc813ea254
Gzim <gzim.asani@gig-ext.com>
2024-01-24 22:52:33 +0100

    add swagger docs

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ require('express-async-errors');
 const helmet = require('helmet');
 const cors = require('cors')
 const xss = require('xss-clean')
-const rateLimiter  = require('express-rate-limit')
+const rateLimiter  = require('express-rate-limit');
+
+// Swager
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDoument = YAML.load('./swagger.yaml')
 
 const express = require('express');
 const app = express();
@@ -30,9 +35,14 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+
+
+
 app.get("/", (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoument));
 
 
 // routes
